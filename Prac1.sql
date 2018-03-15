ea6b8054c106cc598d15c7e8f6298d53eb99a58f
Unknown <aitoraldava@gmail.com>
2018-03-15 12:26:09 +0100

    Asdf

--- Prac1.sql
+++ Prac1.sql
@@ -2,27 +2,41 @@
 
 --EJER1
 CREATE OR REPLACE TRIGGER ins_emple
-BEFORE INSERT ON emple
+  BEFORE INSERT ON emple
 BEGIN
   IF (TO_CHAR(SYSDATE,'HH24:MI') NOT BETWEEN '09:30' AND '17:30')
-  OR (TO_CHAR(SYSDATE,'DY') IN ()
+  OR (TO_CHAR(SYSDATE,'DY') IN ('SAB', 'DOM'))
+    THEN RAISE_APPLICATION_ERROR (-20001, 'Solo se puede añadir personal entre algún tiempo');
+  END IF;
 END ins_emple;
 
 --EJER2
 CREATE OR REPLACE TRIGGER Control_Empleados
-AFTER INSERT ON DELETE OR UPDATE ON emple
-FOR EACH ROW
+  AFTER INSERT OR DELETE OR UPDATE ON emple
+  FOR EACH ROW
 BEGIN
-  CASE
-    WHEN INSERTING THEN
-      INSERT INTO Ctrl_Empleados (Tabla, Usuario,Fecha, Oper)
-      VALUES('Empleados', USER, SYSDATE. 'INSERT')
-    WHEN UPDATING THEN
-      INSERT INTO Ctrl_Empleados (Tabla, Usuario,Fecha, Oper)
-      VALUES('Empleados', USER, SYSDATE. 'UPDATE')
-    WHEN DELETING THEN
-      INSERT INTO Ctrl_Empleados (Tabla, Usuario,Fecha, Oper)
-      VALUES('Empleados', USER, SYSDATE. 'DELETE')
-    ELSE RAISE_APPLICATION_ERROR (-20001, 'ERROR DESCONOCIDO');
-  END CASE;
+  IF INSERTING THEN
+    INSERT INTO Ctrl_Empleados (Tabla, Usuario,Fecha, Oper)
+    VALUES('Empleados', USER, SYSDATE, 'INSERT');
+  ELSIF UPDATING THEN
+    INSERT INTO Ctrl_Empleados (Tabla, Usuario,Fecha, Oper)
+    VALUES('Empleados', USER, SYSDATE, 'UPDATE');
+  ELSIF DELETING THEN
+    INSERT INTO Ctrl_Empleados (Tabla, Usuario,Fecha, Oper)
+    VALUES('Empleados', USER, SYSDATE, 'DELETE');
+  ELSE RAISE_APPLICATION_ERROR (-20001, 'ERROR DESCONOCIDO');
+  END IF;
 END Control_Empleados;
+
+--EJER3
+CREATE OR REPLACE TRIGGER no_emple_ventas
+  v_nombre depart.dnombre%TYPE;
+  BEFORE INSERT ON depart
+  FOR EACH ROW
+BEGIN
+  SELECT dnombre INTO v_nombre FROM depart;
+  IF (UPPER(dnombre) = 'VENTAS')
+    THEN RAISE_APPLICATION_ERROR (-20500, 'No se puden insertar empleados en este departamento');
+  END IF;
+END no_emple_ventas;
+
